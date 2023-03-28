ca7ae9414b1528fe9c5a6ea7e65e90fda4779f65
Eduardo Sampaio <edupcsampaio@gmail.com>
2023-03-28 15:11:48 -0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export function Header() {
+  return (
+    <header className="flex items-center justify-between max-w-[1180px] mx-auto p-6">
+      <div className="flex items-center gap-8">
+        <Image
+          src="/logo.svg"
+          width={94}
+          height={32}
+          alt="Logo escrito Brand"
+        />
+
+        <div className="w-[1px] h-6 bg-gray-800" />
+
+        <nav className="flex items-center gap-6 text-gray-600 font-medium">
+          <Link href="/" className="hover:text-gray-800 transition-colors">
+            Início
+          </Link>
+          <a href="#" className="hover:text-gray-800 transition-colors">
+            Imóveis
+          </a>
+          <a href="#" className="hover:text-gray-800 transition-colors">
+            Contato
+          </a>
+        </nav>
+      </div>
+
+      <Link
+        href="/login"
+        className="bg-gray-800 text-gray-100 py-3 px-6 font-medium text-sm hover:bg-gray-900 transition-colors"
+      >
+        Fazer Login
+      </Link>
+    </header>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,9 @@
+import { Header } from '@/components/Header';
+
 export default function Home() {
-  return <h1 className="text-2xl font-bold">Hello World</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
